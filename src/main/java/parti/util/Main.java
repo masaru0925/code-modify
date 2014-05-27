9cfb43c7a6b2e08a29dfbb944b5997f2fa5c0977
msaiki <msaiki@DFV-NOTE-219.dfv>
2014-05-27 10:59:30 +0900

    Facade Method生成コード追加

--- src/main/java/parti/util/Main.java
+++ src/main/java/parti/util/Main.java
@@ -33,10 +33,11 @@ public class Main {
 				String filePath = "src/main/java/parti/util/pojo/Message.xml";
 				File file = new File(filePath);
 				ReturnEntity entity = JAXB.unmarshal(file, ReturnEntity.class);
-				EntityModify modify = main.createEntityModify(entity.getQueries());
-				LOG.log(Level.INFO, modify.namedQueries);
-				LOG.log(Level.INFO, modify.staticQueryNames);
-				LOG.log(Level.INFO, modify.staticParamNames);
+				ModifyParts modifyParts = main.createModifyParts(entity);
+				LOG.log(Level.INFO, modifyParts.namedQueries);
+				LOG.log(Level.INFO, modifyParts.staticQueryNames);
+				LOG.log(Level.INFO, modifyParts.staticParamNames);
+				LOG.log(Level.INFO, modifyParts.methods);
 
 				List lines = Files.readAllLines(Paths.get(filePath), Charset.forName("UTF-8"));
 //		String source = 
@@ -48,11 +49,26 @@ public class Main {
 		 * とかでやるか
 		 * それとも//CodeModify などを打ちまくってこれがあったら次の行が対象みたいにするか
 		 */
-		private EntityModify createEntityModify(List<Query> queries) {
+		private ModifyParts createModifyParts(ReturnEntity entity) {
+				List<Query> queries = entity.getQueries();
 				StringBuilder namedQueryBuilder = new StringBuilder();
 				StringBuilder staticQueryNameBuilder = new StringBuilder();
 				StringBuilder staticParamNameBuilder = new StringBuilder();
+				StringBuilder methodBuilder = new StringBuilder();
+				StringBuilder staticQueryName_VarNameBuilder = new StringBuilder()
+								.append(entity.getClassName())
+								.append(".QUERY_")
+					;
+				StringBuilder staticParamName_VarNameBuilder = new StringBuilder()
+								.append(entity.getClassName())
+								.append(".PARAM_")
+					;
+			
 				for (Query query : queries) {
+						StringBuilder methodSetParamBuilder = new StringBuilder();
+						staticQueryName_VarNameBuilder
+								.append(query.getName())
+							;
 						namedQueryBuilder
 								.append("@NamedQuery(name=\"")
 								.append(query.getName())
@@ -60,41 +76,78 @@ public class Main {
 								.append(query.getStatement()
 										.replaceAll("\r\n", "")
 										.replaceAll("\n", ""))
-								.append("\")\n");
+								.append("\")\n")
+							;
 						staticQueryNameBuilder
-								.append("public static final String QUERY_")
-								.append(query.getName())
+								.append("public static final String ")
+								.append(staticQueryName_VarNameBuilder.toString())
 								.append(" = \"")
 								.append(query.getName())
-								.append("\";\n");
+								.append("\";\n")
+							;
+						methodBuilder
+							.append("public List<")
+							.append(entity.getClassName())
+							.append("> ")
+							.append(query.getName())
+							.append("(")
+							;
+
 						// TODO: Parameterも使ってメソッドを作る
 						if (null != query.getParameters()) {
 								for (Parameter param : query.getParameters()) {
+										staticParamName_VarNameBuilder
+											.append(param.getName())
+											;
 										staticParamNameBuilder
-												.append("public static final String PARAM_")
-												.append(query.getName())
-												.append("_")
-												.append(param.getName())
+												.append("public static final String ")
+												.append(staticParamName_VarNameBuilder.toString())
 												.append(" = \"")
 												.append(param.getName())
-												.append("\";\n");
+												.append("\";\n")
+											;
+										methodBuilder
+											.append(param.getType())
+											.append(" ")
+											.append(param.getName())
+											.append(", ")
+											;
+										methodSetParamBuilder
+											.append("\tquery.setParam(")
+											.append(staticParamName_VarNameBuilder.toString())
+											.append(", ")
+											.append(param.getName())
+											.append(");\n")
+											;
 								}
 						}
+						methodBuilder
+							.append("){\n")
+							.append("\tQuery query = getEntityManager().createNamedQuery(")
+							.append(staticQueryName_VarNameBuilder.toString())
+							.append(", ")
+							.append(entity.getClassName())
+							.append(".class);\n")
+							.append(methodSetParamBuilder.toString())
+							.append("\treturn query.getResultList();\n")
+							.append("}\n\n")
+							;
 				}
-				EntityModify modify = new EntityModify();
+				ModifyParts modify = new ModifyParts();
 				modify.namedQueries = namedQueryBuilder.toString();
 				modify.staticQueryNames = staticQueryNameBuilder.toString();
 				modify.staticParamNames = staticParamNameBuilder.toString();
+				modify.methods = methodBuilder.toString();
 
 				return modify;
 		}
 
-		class EntityModify {
+		class ModifyParts {
 
 				String namedQueries;
 				String staticQueryNames;
 				String staticParamNames;
-				FacadeModify facadeModify;
+				String methods;
 
 		}
 
